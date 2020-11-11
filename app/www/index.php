c6f38b431312396e0c76f4034a959b36ac1f873d
NeySlim <lio@pew.pet>
2020-11-11 17:39:26 +0100

    Update index.php

--- app/www/index.php
+++ app/www/index.php
@@ -36,7 +36,7 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Streamtool panel by NeySlim</title>
+    <title>Streamtool</title>
 
     <!-- Bootstrap core CSS -->
 
@@ -100,7 +100,7 @@ if (isset($_POST['submit'])) {
                         <div>
 
 
-                            <p>&copy;2020 All Rights Reserved Streamtool by <a href="https://github.com/NeySlim/streamtool"
+                            <p>&copy;2020 Streamtool - <a href="https://github.com/NeySlim/streamtool"
                                                                                   target="_blank">NeySlim</a></p>
                         </div>
                     </div>
